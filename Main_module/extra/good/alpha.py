7e3c73af27d0512311a01265ac7e0e4a507f03b2
Jettas-notorious <jetrocleophas1@gmail.com>
2025-09-10 14:03:41 +0200

    jvgjvjh

---
+++ Main_module/extra/good/alpha.py
@@ -0,0 +1,6 @@
+#!/user/bin/env Python 3
+"""module: alpha"""
+def funA():
+    return "Alpha"
+if _name_=="_main_":
+    print("I prefer to be a module")
---
+++ Main_module/modules.py
@@ -0,0 +1,6 @@
+from platform import machine,platform,version,processor
+
+print(machine())
+print(platform())
+print(version())
+print(processor())
